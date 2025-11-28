b039d5b11c10b6fbf88f6a2ad02b899f34a79c08
Matheus Lofy <145722472+EuMesmoMatheus@users.noreply.github.com>
2025-11-28 00:47:58 -0300

    Fix comment formatting in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-// CI/CD + SonarCloud + Railway ativado – 28/11/2025
+// CI/CD + SonarCloud + Railway ativado – 28/11/2025 *
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
